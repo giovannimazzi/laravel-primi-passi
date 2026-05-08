4329c9b0e1805457bef4b828d599cb67882c44d9
giovannimazzi <gioelor@libero.it>
2026-05-08 12:45:31 +0200

    add view home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="description"
+      content="Master Boolean in Web Development [152] - Specializzazione PHP/Laravel - Assegnazione L06
+"
+    />
+    <meta name="author" content="Giovanni Mazzi" />
+    <title>EX - Primi passi con Laravel</title>
+
+    <!-- Icona Progetto -->
+    <!-- <link rel="icon" href="./assets/img/..." /> -->
+    <link
+      rel="icon"
+      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💻</text></svg>"
+    />
+
+    <!-- Bootstrap Icons -->
+    <!-- <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"
+    /> -->
+
+    <!-- Google Fonts -->
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://..."
+      rel="stylesheet"
+    /> -->
+
+    <!-- Bootstrap CSS -->
+     <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body class="bg-secondary">
+    <nav class="navbar bg-black bg-gradient">
+      <div class="container-fluid">
+        <a class="navbar-brand text-light" href="{{ route('home') }}">
+          <big>💻</big>Primi passi con Laravel
+        </a>
+      </div>
+    </nav>
+    <div class="container mt-5">
+      <h1>HELLO WORLD!</h1>
+    </div>
+  </body>
+</html>
